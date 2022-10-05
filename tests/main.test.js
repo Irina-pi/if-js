ae4422f1f17fb250ee4668c103da10e18f2306a0
Irina Bocharova <rabotairinabocharova@yandex.ru>
2022-10-05 16:29:21 +0300

    corrected function and test but not result

---
+++ tests/main.test.js
@@ -0,0 +1,10 @@
+import { randNumber } from '@ngneat/falso';
+import { sum } from '../src/scripts/main.js';
+
+
+test('When adding new valid product, get successful confirmation', () => {
+  const a = randNumber();
+
+  expect(sum(a, b)).toBe();
+  expect(sum(a, b)).toBe();
+});
