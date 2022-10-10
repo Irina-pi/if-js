9affdf8340c8b71c3fe4c726a712d38d987f10a6
Irina Bocharova <rabotairinabocharova@yandex.ru>
2022-10-10 14:45:53 +0300

    maked tasks for lesson 17 but without tests.

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,10 +1,10 @@
-import { randNumber } from '@ngneat/falso';
-import { sum } from '../src/scripts/main.js';
-
-
-test('When adding new valid product, get successful confirmation', () => {
-  const a = randNumber();
-
-  expect(sum(a, b)).toBe();
-  expect(sum(a, b)).toBe();
-});
+// import { randNumber } from '@ngneat/falso';
+// import { sum } from 'src/scripts/main.js;
+//
+//
+// test('When adding new valid product, get successful confirmation', () => {
+//   const x = randNumber();
+//
+//   expect(sum(x, b)).toBe();
+//   expect(sum(x, b)).toBe();
+// });
