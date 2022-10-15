60c0c0224ac2df96f202ad9263c344bcad07b7af
Irina Bocharova <rabotairinabocharova@yandex.ru>
2022-10-15 12:25:46 +0300

    maked tasks of lesson-18

--- main.js
+++ main.js
@@ -44,45 +44,77 @@ for (let i = 0; i < arr9.length; i++) {
     }
 }
 
-//task 16.5
-function polyndrome (poly) {
-  for (let i = 0; i <= poly.length; i++) {
-    if (poly[i] !== poly[poly.length - (i + 1)]) {
-      return false;
-    } else {
-      return true;
+//task 10
+const arr10 = [2, 5, 8, 15, 0, 6, 20, 3];
+
+for (let i = 0; i < arr10.length; i++) {
+    if (arr10[i] % 2 === 0) {
+        console.log(arr10[i]);
     }
-  }
 }
 
-console.log(polyndrome('шалаш'));
-console.log(polyndrome('ирина'));
+//task 18.5
+const newFormatData = function (dataFun) {
+    return dataFun.replace(/(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/g, '$<day>.$<month>.$<year>');
+}
+
+const dataF = '2020-11-26'
+console.log(newFormatData(dataF));
 
 
-//task 16.6
-//     console.log(max(num5, num10));
-//
-//     const minmax = (num5 > num10) ? num10 : num5;
+//task 18.6
+const data = [
+    {
+        country: 'Russia',
+        city: 'Saint Petersburg',
+        hotel: 'Hotel Leopold',
+    },
+    {
+        country: 'Spain',
+        city: 'Santa Cruz de Tenerife',
+        hotel: 'Apartment Sunshine',
+    },
+    {
+        country: 'Slowakia',
+        city: 'Vysokie Tatry',
+        hotel: 'Villa Kunerad',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hostel Friendship',
+    },
+    {
+        country: 'Indonesia',
+        city: 'Bali',
+        hotel: 'Ubud Bali Resort&SPA',
+    },
+    {
+        country: 'Netherlands',
+        city: 'Rotterdam',
+        hotel: 'King Kong Hostel',
+    },
+    {
+        country: 'Marocco',
+        city: 'Ourika',
+        hotel: 'Rokoko Hotel',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hotel Rehberge Berlin Mitte',
+    },
+];
 
-//task 16.7
-const arr167 = [5, 20, 54, 26, 70, 60, 0, 56, 0, 100];
-
-const zero = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 10 === 0 && array[i] !== 0) {
-      const y = array[i] / 10;
-      array[i] = y + 'zero';
-    } else if (array[i] % 100 === 0 && array[i] !== 0 ) {
-      const n = array[i] / 100;
-      array[i] = n + 'zero' + 'zero';
-    } else if (array[i] === 0) {
-      array[i] = 'zero';
-    } else {
-      array[i];
+function seachByWorld(world = '') {
+    for (let i = 0; i < data.length; i++) {
+        result = data[i].country + ', ' + data[i].city + ', ' + data[i].hotel;
+        if (data[i].city === world || data[i].country === world || data[i].hotel === world) {
+            return result;
+        }
     }
-  }
-  return array;
-};
+}
 
-zero(arr167);
-console.log(arr167);
+console.log(seachByWorld('Germany'));
+console.log(seachByWorld('please'));
+console.log(seachByWorld('King Kong Hostel'));
