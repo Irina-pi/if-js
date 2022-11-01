f6652df47320e3838b8a3b976b73d52aec3fc9ec
Irina Bocharova <rabotairinabocharova@yandex.ru>
2022-11-01 03:10:32 +0300

    corrected comitting code

--- main.js
+++ main.js
@@ -44,6 +44,15 @@ for (let i = 0; i < arr9.length; i++) {
     }
 }
 
+//task 10
+let arr10 = [2, 5, 8, 15, 0, 6, 20, 3];
+
+for (let i = 0; i < arr10.length; i++) {
+  if (arr10[i] % 2 === 0) {
+    console.log(arr10[i]);
+  }
+}
+
 //task 16.5
 function polyndrome (poly) {
   for (let i = 0; i <= poly.length; i++) {
@@ -59,10 +68,13 @@ console.log(polyndrome('шалаш'));
 console.log(polyndrome('ирина'));
 
 
-//task 16.6
-//     console.log(max(num5, num10));
+// task 16.6
+// let num5 = 5;
+// let num10 = 10;
 //
-//     const minmax = (num5 > num10) ? num10 : num5;
+// console.log(max(num5, num10));
+//
+// const minmax = (num5 > num10) ? num10 : num5;
 
 //task 16.7
 const arr167 = [5, 20, 54, 26, 70, 60, 0, 56, 0, 100];
