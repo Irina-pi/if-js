37b57cc09153fc7b7de7fa62fbbd27cbb2b3a495
Irina Bocharova <rabotairinabocharova@yandex.ru>
2023-01-26 00:35:45 +0300

    I made function uniqueCountry correctly

--- src/main.js
+++ src/main.js
@@ -165,20 +165,6 @@ const hotels = [
   },
 ];
 
-// function searchByWord(word = '') {
-//   const copyHotels = [...hotels];
-//   const hotelsFilter = copyHotels.filter((hotel) => {
-//     return hotel.name === word || hotel.city === word || hotel.country === word;
-//   });
-//   const result = hotelsFilter.map((item) => {
-//     return `${item.country}, ${item.city}, ${item.name}`;
-//   });
-//   if (hotelsFilter.length === 0) {
-//     return 'Search didn`t get results';
-//   }
-//   return result.join('; ');
-// }
-
 function searchByWord(word = '') {
   const result = hotels.reduce((acc, hotel) => {
     if (hotel.name === word || hotel.city === word || hotel.country === word) {
@@ -196,175 +182,19 @@ console.log(searchByWord('Germany'));
 console.log(searchByWord('Poland'));
 console.log(searchByWord('City'));
 
+console.log('------------------------------------------');
 //task 19.7
 
-// function allCity(hotels = hotels) {
-//   let h = hotels.forEach(index => console.log(index));
-//   const result = {};
-//   if()
-//
-// }
-//
-// allCity(hotels);
-//
-//
-// console.log(hotels.findIndex((item) => item.country === 'Germany'));
-// console.log(hotels.findIndex((item) => item.country === 'Germany'));
-// console.log(hotels.findIndex((item) => item.country === 'UK'));
-
-//
-// function cityOfCountry(hotels) {
-//
-//   // const hotelsFilter = hotels.filter((hotel) => {
-//   //   return hotel.country === hotel.country;
-//   // });
-//   const resArr = [];
-//   hotels.forEach((obj) => {
-//     const country = obj.country;
-//     // const hotelsFilter = hotels.filter((hotel) => {
-//     //   return hotel.country === hotel.country;
-//     // });
-//     // const city = obj.city;
-//       if(hotels.country === obj.country) {
-//         resArr.push(obj);
-//       }
-//   })
-//   return resArr;
-// }
-//
-// console.log(cityOfCountry(hotels));
-
-  hotels.forEach((index) => {
-    const fil = hotels.filter((item ) => {
-      return item.country === index.country;
-    })
-    fil.map((item) => {
-      return [item.country, item.city];
-    })
-  })
-
-console.log(hotels);
-
-const uniqueCountry = hotels.reduce((acc, {country, city},  index) => {
-  acc[country] = hotels[index].city;
-
-  return acc;
-}, {});
-console.log(uniqueCountry);
-
-// const uniqueCountry = hotels.reduce((acc, {country, city},  index) => {
-//   // acc[country] = [];
-//   for (let i = 0; i < hotels.length; i++) {
-//     const f = hotels.filter((hotel) => {
-//       if(hotel.country === hotels[i].country) {
-//         return f.push(hotels[i].city);
-//       }
-//     })
-//   }
-//   // const dubl = hotels.filter((hotel, index, array) => {
-//   //   return array.indexOf(hotel) !== index;
-//   // })
-//
-//   // return ;
-// }, {});
-// console.log(uniqueCountry);
-
-0
-
-const ex = hotels.reduce((acc, {country, city}, index) => {
-  acc[country] = acc[country] ? acc[country] = [city, city] : acc[country] = [city];
+function uniqueCountry(array) {
+  const arrayReduce = array.reduce((acc, { country, city }) => {
+    acc[country]
+      ? !acc[country].includes(city) && acc[country].push(city)
+      : (acc[country] = [city]);
 
     return acc;
   }, {});
 
-console.log(ex);
-
-
-
-
-
-const copHotels = [...hotels];
-
-
-
-// const x = hotels.reduce((acc, hotel) => {
-//  acc[hotel.country] = acc[hotel.country] ?
-//    (acc[hotel.country] = [hotel.city, [hotel.city].push([hotel.city])]) : (acc[hotel.country] = [hotel.city]);
-//     return acc;
-// }, {});
-// console.log(x);
-//
-// const x = hotels.reduce((acc, { country, city }) => {
-//   acc[country] = acc[country] ?
-//     (acc[country] = [city, [city.push(city)]) : (acc[country] = city);
-//   return acc;
-// }, {});
-// console.log(x);
-
-console.log('------------------------------------------');
-
-const rCity = hotels.reduce((acc, {country, city}, index) => {
-  acc[country] = [];
-  acc[country].push(city);
-
-
-  // const find = acc[country] === acc[country];
-  // if (acc[country] === find) {
-  //   acc[country].push(country, city);
-  // }
-
-
-  return acc;
-}, {});
-
-console.log(rCity);
-
-
-//
-// const filterCountry = hotels.filter((hotel, index) => {
-//   return hotels[index].country === hotel.country;
-// });
-// console.log(filterCountry);
-
-// const examplFun = hotels => Object.keys(hotels).reduce((result, key) => {
-//   let nameObj = hotels[key].country;
-//   let lengthObj = hotels[key].city;
-//
-//   if(lengthObj >= nameObj) {
-//     result[key] = hotels[key];
-//   }
-//
-//   return result;
-// }, {})
-
-console.log('------------------------------------------');
-
-// const copyHotelsObj = {...copyHotels};
-// console.log(copyHotelsObj);
-
-const uniqueCity = (array) => {
-  const copyArray = [...array];
-  let arrAllCountry = [];
-  copyArray.forEach((item) => arrAllCountry.push(item.country));
-  const arrUniqCountry = arrAllCountry.filter(function (item, item2) {
-    return arrAllCountry.indexOf(item) === item2;
-  });
-  let objUniqCountry = {...arrUniqCountry};
-  return objUniqCountry;
+  return arrayReduce;
 }
 
-console.log(uniqueCity(hotels));
-
-const countryObj = {}
-console.log('------------------------------------------');
-
-// const copyHotels = [...hotels];
-// let arrCountry = [];
-//
-// copyHotels.forEach((item) => arrCountry.push(item.country))
-// console.log(arrCountry);
-//
-// const countryArray = arrCountry.filter(function (item, item2) {
-//   return arrCountry.indexOf(item) === item2;
-// });
-// console.log(countryArray);
+console.log(uniqueCountry(hotels));
