766fb27aca816a5d3c1b2b094e8c9fab76fff4a9
Irina Bocharova <rabotairinabocharova@yandex.ru>
2023-10-05 16:36:00 +0300

    Made nev function for block-three on base to fetch. Delete mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,70 +1,71 @@
-import { data } from './data.js';
+// import { data } from './data.js';
 
-const funCardsHotelForId = function (idFun) {
-  const findHotelFun = data.find((item) => item.id === idFun);
-  return findHotelFun;
-};
-
-const createSliderItem = function (id) {
-  const xId = funCardsHotelForId(id);
-  const divId = document.getElementById(id);
-  const imgEl = divId.children[0].children[0];
-  imgEl.setAttribute('src', xId.imageUrl);
-
-  const name = divId.children[1];
-  name.textContent = xId.name;
-
-  const location = divId.children[2];
-  location.textContent = `${xId.city}, ${xId.country}`;
-};
-
-createSliderItem('71ce9eac-e9b9-44f0-a342-9ff0b565f3b7');
-createSliderItem('aa560608-a879-48a7-80b6-deff2806b250');
-createSliderItem('1d88fefe-edf1-4cda-844a-babbf29bb2b3');
-createSliderItem('a2bf824d-edd8-41f0-8b70-244334086ab4');
-
-// const findHotel = data.find((item) => item.name === 'Villa Kunerad');
-// console.log(findHotel);  //{id: '1d88fefe-edf1-4cda-844a-babbf29bb2b3', name: 'Villa Kunerad', city: 'Vysokie Tatry', country: 'Slowakia', imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg'}
-// const filterHotel = data.filter((item) => item.name === 'Villa Kunerad');
-// console.log(filterHotel);  //[{…}]
-
-// const funCardsHotel = function (hotel) {
-//   const findHotelFun = data.find((item) => item.name === hotel);
+// const funCardsHotelForId = function (idFun) {
+//   const findHotelFun = data.find((item) => item.id === idFun);
 //   return findHotelFun;
 // };
+
+// const createSliderItem = function (id) {
+//   const xId = funCardsHotelForId(id);
+//   const divId = document.getElementById(id);
+//   const imgEl = divId.children[0].children[0];
+//   imgEl.setAttribute('src', xId.imageUrl);
 //
-// const funMake = function (hotelFun, imgElFunId, nameElFunId, locationElFunId) {
-//   const x = funCardsHotel(hotelFun);
-//   const imgElFun = document.getElementById(imgElFunId);
-//   const nameElFun = document.getElementById(nameElFunId);
-//   const locationElFun = document.getElementById(locationElFunId);
-//
-//   imgElFun.setAttribute('src', x.imageUrl);
-//   nameElFun.textContent = x.name;
-//   locationElFun.textContent = x.city + ', ' + x.country;
+//   const name = divId.children[1];
+//   name.textContent = xId.name;
+//   const location = divId.children[2];
+//   location.textContent = `${xId.city}, ${xId.country}`;
 // };
-//
-// funMake(
-//   'Hotel Leopold',
-//   'imgHotelLeopold-js',
-//   'nameHotelLeopold-js',
-//   'locationHotelLeopold-js',
-// );
-// funMake(
-//   'Apartment Sunshine',
-//   'imgApartSunshine-js',
-//   'nameApartSunshine-js',
-//   'locationApartSunshine-js',
-// );
-// funMake(
-//   'Villa Kunerad',
-//   'imgVillaKunerad-js',
-//   'nameVillaKunerad-js',
-//   'locationVillaKunerad-js',
-// );
-// funMake(
-//   'Hostel Friendship',
-//   'imgHostelFriendship-js',
-//   'nameHostelFriendship-js',
-//   'locationHostelFriendship-js',
-// );
+
+// createSliderItem('71ce9eac-e9b9-44f0-a342-9ff0b565f3b7');
+// createSliderItem('aa560608-a879-48a7-80b6-deff2806b250');
+// createSliderItem('1d88fefe-edf1-4cda-844a-babbf29bb2b3');
+// createSliderItem('a2bf824d-edd8-41f0-8b70-244334086ab4');
+
+const createSliderItem = function (obj) {
+  const divBlockEl = document.getElementById('mainDivHomesGuestLoves-js');
+
+  const divMainEl = document.createElement('div');
+  divMainEl.setAttribute('id', `${obj.id}`);
+  divMainEl.setAttribute('class', 'block-three__img');
+
+  const divImgEl = document.createElement('div');
+  divImgEl.setAttribute('class', 'block-three__img block-three__img--hotel');
+
+  const imgEl = document.createElement('img');
+  imgEl.setAttribute('class', 'block-three__img');
+  imgEl.setAttribute('alt', 'Hotel');
+  imgEl.setAttribute('src', `${obj.imageUrl}`);
+
+  const divCaptionEl = document.createElement('div');
+  divCaptionEl.setAttribute(
+    'class',
+    'block-three__caption block-three__caption--hotel',
+  );
+  divCaptionEl.textContent = obj.name;
+
+  const divLocationEl = document.createElement('div');
+  divLocationEl.setAttribute(
+    'class',
+    'block-three__location block-three__location--hotel',
+  );
+  divLocationEl.textContent = `${obj.city}, ${obj.country}`;
+
+  divMainEl.appendChild(divImgEl).appendChild(imgEl);
+  divMainEl.appendChild(divCaptionEl);
+  divMainEl.appendChild(divLocationEl);
+  divBlockEl.appendChild(divMainEl);
+};
+
+fetch('https://if-student-api.onrender.com/api/hotels/popular')
+  .then((response) => {
+    return response.json();
+  })
+  .then((arr) => {
+    arr.forEach((obj) => {
+      createSliderItem(obj);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
