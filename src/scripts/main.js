53bf89e5eac90ad51e28ca1a3c7ded48f648fce6
Irina Bocharova <rabotairinabocharova@yandex.ru>
2023-10-18 13:21:44 +0300

    made functions for new block twoSearch-screen

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,33 +22,36 @@
 // createSliderItem('1d88fefe-edf1-4cda-844a-babbf29bb2b3');
 // createSliderItem('a2bf824d-edd8-41f0-8b70-244334086ab4');
 
-const createSliderItem = function (obj) {
-  const divBlockEl = document.getElementById('mainDivHomesGuestLoves-js');
+const createSliderItem = function (
+  obj,
+  divID,
+  divMainElClass,
+  divImgElClass,
+  imgElClass,
+  imgElAlt,
+  divCaptionElClass,
+  divLocationElClass,
+) {
+  const divBlockEl = document.getElementById(divID);
 
   const divMainEl = document.createElement('div');
   divMainEl.setAttribute('id', `${obj.id}`);
-  divMainEl.setAttribute('class', 'block-three__img');
+  divMainEl.setAttribute('class', divMainElClass);
 
   const divImgEl = document.createElement('div');
-  divImgEl.setAttribute('class', 'block-three__img block-three__img--hotel');
+  divImgEl.setAttribute('class', divImgElClass);
 
   const imgEl = document.createElement('img');
-  imgEl.setAttribute('class', 'block-three__img');
-  imgEl.setAttribute('alt', 'Hotel');
+  imgEl.setAttribute('class', imgElClass);
+  imgEl.setAttribute('alt', imgElAlt);
   imgEl.setAttribute('src', `${obj.imageUrl}`);
 
   const divCaptionEl = document.createElement('div');
-  divCaptionEl.setAttribute(
-    'class',
-    'block-three__caption block-three__caption--hotel',
-  );
+  divCaptionEl.setAttribute('class', divCaptionElClass);
   divCaptionEl.textContent = obj.name;
 
   const divLocationEl = document.createElement('div');
-  divLocationEl.setAttribute(
-    'class',
-    'block-three__location block-three__location--hotel',
-  );
+  divLocationEl.setAttribute('class', divLocationElClass);
   divLocationEl.textContent = `${obj.city}, ${obj.country}`;
 
   divMainEl.appendChild(divImgEl).appendChild(imgEl);
@@ -63,7 +66,59 @@ fetch('https://if-student-api.onrender.com/api/hotels/popular')
   })
   .then((arr) => {
     arr.forEach((obj) => {
-      createSliderItem(obj);
+      createSliderItem(
+        obj,
+        'mainDivHomesGuestLoves-js',
+        'block-three__img',
+        'block-three__img block-three__img--hotel',
+        'block-three__img',
+        'Hotel',
+        'block-three__caption block-three__caption--hotel',
+        'block-three__location block-three__location--hotel',
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+fetch('https://if-student-api.onrender.com/api/hotels', {
+  method: 'GET',
+  search: '',
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((arr) => {
+    const formEl = document.getElementById('form-js');
+    formEl.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const twoSearchScreenEl = document.getElementById('twoSearch-screen-js');
+      twoSearchScreenEl.classList.remove('twoSearch-screen');
+
+      const formData = new FormData(formEl);
+      const value = formData.get('block_one_input_city');
+
+      const arrSearch = arr.filter(
+        (search) =>
+          search.name.toLowerCase().includes(value.toLowerCase()) ||
+          search.city.toLowerCase().includes(value.toLowerCase()) ||
+          search.country.toLowerCase().includes(value.toLowerCase()),
+      );
+
+      arrSearch.forEach((arrSearch) => {
+        createSliderItem(
+          arrSearch,
+          'mainDivAvailableHotels-js',
+          'block-twoSearch__img',
+          'block-twoSearch__img block-twoSearch__img--hotel',
+          'block-twoSearch__img',
+          'Hotel',
+          'block-twoSearch__caption block-twoSearch__caption--hotel',
+          'block-twoSearch__location block-twoSearch__location--hotel',
+        );
+      });
     });
   })
   .catch((err) => {
